baedab9a6dc86d6c8f92fd5e2d2aa04522892495
Mark <markmccoid@gmail.com>
2020-06-08 23:55:42 -0400

    Add Genres and Release Date

--- src/components/search/SearchResultItem.js
+++ src/components/search/SearchResultItem.js
@@ -10,41 +10,75 @@ import {
 import { MaterialIcons } from '@expo/vector-icons';
 
 const SearchResultItem = ({ movie, saveMovie }) => {
+  console.log('search result Item', movie.releaseDate);
   return (
     <View
       style={[styles.container, movie.existsInSaved && styles.existsInSaved]}
     >
       <View style={styles.movieInfo}>
         <Text style={styles.title}>{movie.title}</Text>
+        <View style={styles.genreContainer}>
+          {movie.genres.map((genre) => (
+            <Text style={styles.genre}>{genre}</Text>
+          ))}
+        </View>
+        <View style={styles.dateContainer}>
+          <Text style={styles.dateText}> {movie.releaseDate.formatted}</Text>
+        </View>
         <Text>{movie.overview}</Text>
         {!movie.existsInSaved && (
           <Button title="Add" onPress={() => saveMovie(movie)} />
         )}
       </View>
-      <Image
-        source={{ url: movie.posterURL }}
-        style={styles.image}
-        PlaceholderContent={<MaterialIcons name="broken-image" size={64} />}
-      />
+      {movie.posterURL ? (
+        <Image
+          source={{ url: movie.posterURL }}
+          style={styles.image}
+          PlaceholderContent={<MaterialIcons name="broken-image" size={64} />}
+        />
+      ) : (
+        <View style={[styles.image, styles.imageBackup]}>
+          <Text style={styles.imageBackupText}>{movie.title}</Text>
+        </View>
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 2,
     flexDirection: 'row',
     margin: 5,
     borderColor: 'black',
     borderWidth: 1,
     padding: 5,
+    backgroundColor: '#ecf0f1',
   },
   movieInfo: {
     flex: 1,
-    justifyContent: 'space-between',
   },
   title: {
     fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 2,
+  },
+  genreContainer: {
+    flexDirection: 'row',
+    marginBottom: 10,
+  },
+  genre: {
+    padding: 2,
+    marginHorizontal: 5,
+    borderColor: 'black',
+    borderWidth: 1,
+    fontSize: 12,
+    backgroundColor: '#34495e44',
+  },
+  dateContainer: {
+    marginBottom: 5,
+  },
+  dateText: {
+    fontSize: 14,
     fontWeight: 'bold',
   },
   image: {
@@ -52,6 +86,16 @@ const styles = StyleSheet.create({
     height: 225,
     borderColor: 'black',
     borderWidth: 1,
+    marginLeft: 5,
+  },
+  imageBackup: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageBackupText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
   existsInSaved: {
     borderColor: 'green',
