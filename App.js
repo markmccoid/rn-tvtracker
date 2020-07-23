2b8b4b56f01f1ca68ed7740917c3c48da755ab5e
Mark <markmccoid@gmail.com>
2020-07-23 00:12:56 -0400

    update debug IP

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { Provider } from 'overmind-react';
-import { createOvermind } from 'overmind';
-import { config } from './src/store/overmind';
-import { initTMDB } from '@markmccoid/tmdb_api';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { Provider } from "overmind-react";
+import { createOvermind } from "overmind";
+import { config } from "./src/store/overmind";
+import { initTMDB } from "@markmccoid/tmdb_api";
 
-import { Alert } from 'react-native';
-import RootNav from './src/navigation/RootNav';
+import { Alert } from "react-native";
+import RootNav from "./src/navigation/RootNav";
 
 export const AuthContext = React.createContext();
 
 const App = () => {
-  initTMDB('0e4935aa81b04539beb687d04ff414e3');
-  const overmind = createOvermind(config, { devtools: '192.168.1.31:3031' });
+  initTMDB("0e4935aa81b04539beb687d04ff414e3");
+  const overmind = createOvermind(config, { devtools: "192.168.1.5:3031" });
   return (
     <Provider value={overmind}>
       <NavigationContainer>
