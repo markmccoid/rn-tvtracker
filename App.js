d527913cd268f21628dcd921f72c0155313c0495
Mark <markmccoid@gmail.com>
2020-07-29 23:24:52 -0400

    Other

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ export const AuthContext = React.createContext();
 
 const App = () => {
   initTMDB("0e4935aa81b04539beb687d04ff414e3");
-  const overmind = createOvermind(config, { devtools: "192.168.1.5:3031" });
+  const overmind = createOvermind(config, { devtools: "192.168.1.4:3031" });
   return (
     <Provider value={overmind}>
       <NavigationContainer>
