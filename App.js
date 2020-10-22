1091bc8ff51b0cff82d7b66baa01528705555c54
Mark <markmccoid@gmail.com>
2020-10-22 14:30:14 -0400

    prep for 1.0.1 build

--- App.js
+++ App.js
@@ -11,12 +11,11 @@ import RootNav from "./src/navigation/RootNav";
 // export const AuthContext = React.createContext();
 
 const App = () => {
-  // LogBox.ignoreLogs([
-  //   "Non-serializable values were found in the navigation state"
-  // ]);
+  LogBox.ignoreLogs(["Non-serializable values were found in the navigation state"]);
   // console.log("InApp");
   initTMDB("0e4935aa81b04539beb687d04ff414e3");
-  const overmind = createOvermind(config, { devtools: "192.168.1.13:3031" });
+  // const overmind = createOvermind(config, { devtools: "192.168.1.13:3031" });
+  const overmind = createOvermind(config);
   return (
     <Provider value={overmind}>
       <NavigationContainer>
