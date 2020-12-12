4b4e837789c7824f17f252a1dabb64625bc22475
Mark <markmccoid@gmail.com>
2020-12-12 15:39:18 -0500

    Removed Focus check for Scroll to Top on Dismiss

--- src/screens/view/ViewMovies/ViewMoviesFilterScreen.js
+++ src/screens/view/ViewMovies/ViewMoviesFilterScreen.js
@@ -24,63 +24,6 @@ const ViewMoviesFilterScreen = ({ route, navigation }) => {
     clearFilterGenres,
   } = actions.oSaved;
 
-  // Get the key for the Movies route
-  // Used in teh dismiss listener
-  const moviesKey = useNavigationState((state) => {
-    return state.routes[state.routeNames.indexOf("Movies")].key;
-  });
-
-  // useFocusEffect(
-  //   React.useCallback(() => {
-  //     console.log("UFE-Filter");
-  //     return () => {
-  //       console.log("returning from UEF -> navigating");
-  //       //navigation.navigate("Movies", { returning: true, filterModified: true });
-  //       console.log("returning AFTERfrom UEF -> navigating");
-  //     };
-  //   }, [])
-  // );
-  // //------------------------------
-  // React.useEffect(() => {
-  //   const unsubscribe = navigation.addListener("appear", (e) => {
-  //     console.log("In Filter Appear");
-  //   });
-
-  //   return () => {
-  //     unsubscribe();
-  //     console.log("Unsub - Appear");
-  //   };
-  // }, [navigation]);
-
-  // Listener for when a screen is dismissed using the dismiss gesture
-  // When this happens we set the filterModified Param for the Movies route
-  // Letting the ViewMoviesScreen know that the filter was modified.
-  React.useEffect(() => {
-    const unsubscribe = navigation.addListener("dismiss", (e) => {
-      navigation.dispatch({
-        ...CommonActions.setParams({ filterModified: true }),
-        source: moviesKey,
-      });
-    });
-
-    return () => {
-      unsubscribe();
-      // console.log("Unsub - Dismiss");
-    };
-  }, [navigation]);
-
-  // React.useEffect(() => {
-  //   const unsubscribe = navigation.addListener("transitionStart", (e) => {
-  //     if (e.data.closing) {
-  //       console.log("Will be Dismiss");
-  //     } else {
-  //       console.log("WIll Appear");
-  //     }
-  //   });
-
-  //   return unsubscribe;
-  // }, [navigation]);
-
   //------------------------------
 
   //---TESTING  Probably should be a getter in the store.+
