4934aa65c5a896c414e11bace4b9d3a349741816
Mark <markmccoid@gmail.com>
2020-12-22 23:18:16 -0500

    Bug fix - show tags showing on NON saved movie

--- src/screens/view/ViewDetails/DetailMainInfo.js
+++ src/screens/view/ViewDetails/DetailMainInfo.js
@@ -152,31 +152,33 @@ const DetailMainInfo = ({ movie, isInSavedMovies, viewTags, setViewTags, transit
             ))}
           </View>
         </View>
-        <View
-          style={{
-            justifyContent: "flex-end",
-            alignItems: "center",
-            marginBottom: 10,
-            flexGrow: 1,
-          }}
-        >
-          <Button
-            onPress={() => {
-              if (transitionRef.current) {
-                transitionRef.current.animateNextTransition();
-              }
-              setViewTags((prev) => !prev);
+        {isInSavedMovies && (
+          <View
+            style={{
+              justifyContent: "flex-end",
+              alignItems: "center",
+              marginBottom: 10,
+              flexGrow: 1,
             }}
-            title={viewTags ? "Hide Tags" : "Show Tags"}
-            bgOpacity="ff"
-            bgColor={colors.primary}
-            small
-            // width={100}
-            wrapperStyle={{ borderRadius: 10, paddingLeft: 10, paddingRight: 10 }}
-            color="#fff"
-            noBorder
-          />
-        </View>
+          >
+            <Button
+              onPress={() => {
+                if (transitionRef.current) {
+                  transitionRef.current.animateNextTransition();
+                }
+                setViewTags((prev) => !prev);
+              }}
+              title={viewTags ? "Hide Tags" : "Show Tags"}
+              bgOpacity="ff"
+              bgColor={colors.primary}
+              small
+              // width={100}
+              wrapperStyle={{ borderRadius: 10, paddingLeft: 10, paddingRight: 10 }}
+              color="#fff"
+              noBorder
+            />
+          </View>
+        )}
       </View>
     </View>
   );
