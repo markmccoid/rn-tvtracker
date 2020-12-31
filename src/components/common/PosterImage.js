fb49e2cc6c2e72d5e5ab0559d95a4b307372d361
Mark <markmccoid@gmail.com>
2020-12-31 10:18:56 -0500

    Update to use RN Image with a View fallback

--- src/components/common/PosterImage.js
+++ src/components/common/PosterImage.js
@@ -1,15 +1,33 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { Image } from "react-native-elements";
+import { View, Text, StyleSheet, Image } from "react-native";
+// import { Image } from "react-native-elements";
 
-const PosterImage = ({ uri, placeholderText, posterWidth, posterHeight }) => {
-  const posterURI = uri ? { uri } : {};
+const PosterImage = ({
+  uri,
+  placeholderText,
+  posterWidth,
+  posterHeight,
+  fallbackStyle = { container: {}, text: {} },
+}) => {
+  if (uri) {
+    return (
+      <Image
+        style={styles.posterImage(posterWidth, posterHeight)}
+        source={{ uri }}
+        resizeMode="cover"
+      />
+    );
+  }
   return (
-    <Image
-      source={posterURI}
-      style={styles.posterImage(posterWidth, posterHeight)}
-      PlaceholderContent={<Text style={styles.imageBackupText}>{placeholderText}</Text>}
-    />
+    <View
+      style={[
+        styles.posterImage(posterWidth, posterHeight),
+        styles.fallbackContainer,
+        fallbackStyle.container,
+      ]}
+    >
+      <Text style={[styles.imageBackupText, fallbackStyle.text]}>{placeholderText}</Text>
+    </View>
   );
 };
 
@@ -18,6 +36,13 @@ const styles = StyleSheet.create({
     width,
     height,
   }),
+  fallbackContainer: {
+    backgroundColor: "#ccc",
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "#777",
+  },
   imageBackupText: {
     fontSize: 22,
     fontWeight: "bold",
