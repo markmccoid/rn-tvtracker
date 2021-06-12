734a5447543b662f6983b61348a85741218c9791
Mark <markmccoid@gmail.com>
2021-06-12 13:53:45 -0400

    update of IP

--- src/store/overmind.js
+++ src/store/overmind.js
@@ -19,4 +19,4 @@ export const useOEffects = createEffectsHook();
 export const useOReaction = createReactionHook();
 
 // export const overmind = createOvermind(config);
-export const overmind = createOvermind(config, { devtools: "192.168.1.34:3031" });
+export const overmind = createOvermind(config, { devtools: "192.168.1.26:3031" });
